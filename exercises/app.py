3f297369769db92371d7944f616fae517c16dbff
aboud20-meet <aboud20@meet.mit.edu>
2018-08-11 20:11:23 +0300

    routing

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,14 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'START HERE: CHANGE ME'
+def home_route():
+    return render_template(
+    	"home.html")
+
+app.run(debug=True)
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+	return render_template("student.html", n=student_id)
 
 app.run(debug=True)
